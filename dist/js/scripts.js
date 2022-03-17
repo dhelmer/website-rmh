48add1450a5634adb1c881505b54a22543b6454d
Doug Helmer <dhelmer@gmail.com>
2022-03-16 18:54:52 -0600

    Add copyright year script. Layout tweaks.

---
+++ dist/js/scripts.js
@@ -0,0 +1,4 @@
+function copyright() {
+  let date =  new Date().getFullYear();
+  document.getElementById("copyright").innerHTML = '&copy; Copyright ' + date +'. Munro Publishing. All Rights Reserved.';
+}
