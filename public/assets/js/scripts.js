3ca907e590dc186ab65cce70eec4710906413367
Doug Helmer <dhelmer@gmail.com>
2022-03-17 21:35:47 -0600

    Many stuffs

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,8 +1,3 @@
-// function copyright() {
-//   let date =  new Date().getFullYear();
-//   document.getElementById("copyright").innerHTML = '&copy; Copyright ' + date +'. Munro Publishing. All Rights Reserved.';
-// }
-
 document.onreadystatechange = function(event) {
   if (document.readyState === "complete") {
 
